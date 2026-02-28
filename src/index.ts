d7331cfb61970e454626cc0183a93d080e804013
Levitre <levitre.mathys30@gmail.com>
2026-02-28 19:34:25 +0100

    feat: add cors and dynamic port

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,23 @@
 import express from 'express';
+import cors from 'cors';
 import type { Request, Response } from 'express';
 import { userRouter } from './user/user.route.js';
 
-export const app = express()
+export const app = express();
 
-const domainName: string = 'localhost'
-const PORT: number = 3000
+const PORT: number = Number(process.env.PORT) || 3000;
 
-app.use(express.json())
+app.use(cors({
+  origin: '*' // remplace par ton URL Vercel plus tard ex: 'https://mon-front.vercel.app'
+}));
 
+app.use(express.json());
 app.use('/user', userRouter);
 
 app.get('/', async (_req: Request, res: Response) => {
-  res.status(200).send('Welcome to HTTP server')
-})
+  res.status(200).send('Welcome to HTTP server');
+});
 
 app.listen(PORT, () => {
-  console.log(
-    `Serveur tournant sur le port ${PORT}\nSe rendre sur http://${domainName}:${PORT}`,
-  )
-})
+  console.log(`Serveur tournant sur le port ${PORT}`);
+});
